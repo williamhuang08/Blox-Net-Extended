be37b90841f5d03f12e13b5bc57de8ecb24056f6
Cael Magner <92242939+NotAFood@users.noreply.github.com>
2025-02-10 21:08:28 -0800

    max_workers fixed

--- scripts/run_pipeline_single_obj_parallel.py
+++ scripts/run_pipeline_single_obj_parallel.py
@@ -1,16 +1,19 @@
-from bloxnet.pybullet.place_blocks_in_json import init_pybullet
-from tqdm.contrib.concurrent import process_map
 import os
+
 import dotenv
+from tqdm.contrib.concurrent import process_map
+
+from bloxnet.pipelines.bracket_selection import get_best_assembly
+from bloxnet.pipelines.ratings import get_ratings
+
 # from bloxnet.pipelines.parallel_whole_structure import generate_structure
 from bloxnet.pipelines.whole_structure import generate_structure
 from bloxnet.pybullet.place_blocks_in_json import init_pybullet
 from bloxnet.utils.utils import load_from_json, write_error
-from bloxnet.pipelines.ratings import get_ratings
-from bloxnet.pipelines.bracket_selection import get_best_assembly
 from perturbation_analysis.run_perturbation_analysis import run_perturbation_analysis
 
 structure_name = "Bridge"
+
 
 def make_pybullet(x):
     to_build, num_structures = x
@@ -30,16 +33,15 @@ def make_pybullet(x):
 
     return assembly
 
+
 def main_run_pipeline_single_obj_parallel(to_build, num_structures=10):
     # Ensure this is only run in the main process
     assemblies = process_map(
         make_pybullet,
-        zip(
-            [to_build] * num_structures,
-            range(num_structures)
-        )
-        )
-    
+        zip([to_build] * num_structures, range(num_structures)),
+        max_workers=3,
+    )
+
     assemblies = list(filter(lambda x: x is not None, assemblies))
 
     init_pybullet(gui=False)
@@ -49,13 +51,18 @@ def main_run_pipeline_single_obj_parallel(to_build, num_structures=10):
 
     best_assembly = get_best_assembly(assemblies, use_stability=True)
     if best_assembly is None:
-        write_error(f"{assemblies[0].structure_directory}/error.txt", "best_assembly is None, could mean all designs are unstable")
+        write_error(
+            f"{assemblies[0].structure_directory}/error.txt",
+            "best_assembly is None, could mean all designs are unstable",
+        )
 
     # best_assembly = run_perturbation_analysis(best_assembly)
 
     # best_assembly.save_to_structure_dir(name = "best_assembly.pkl")
     if best_assembly is not None:
-        best_assembly.human_friendly_save(f"{best_assembly.structure_directory}/best_assembly")
+        best_assembly.human_friendly_save(
+            f"{best_assembly.structure_directory}/best_assembly"
+        )
 
 
 if __name__ == "__main__":
